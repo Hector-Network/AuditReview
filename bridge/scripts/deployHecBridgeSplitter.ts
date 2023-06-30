3be30c825fc44e1675badfea36e47d7a1c9d7f4a
Xiang Chu <xiangchu1016@gmail.com>
2023-06-30 19:08:55 -0400

    upd: deployment script for toggle all token contracts

--- bridge/scripts/deployHecBridgeSplitter.ts
+++ bridge/scripts/deployHecBridgeSplitter.ts
@@ -17,6 +17,7 @@ async function main() {
 	const DAO = "0x677d6EC74fA352D4Ef9B1886F6155384aCD70D90";
 	const version = "2.0";
 
+
 	console.log("Deploying contracts with the account:", deployer.address);
 	console.log("Account balance:", (await deployer.getBalance()).toString());
 
@@ -24,11 +25,38 @@ async function main() {
 	console.log("Gas Price: ", gas);
 
 	const chainID = '250'
-	const tokenLimit = 100
+	const tokenLimitQueue = 400
+	const tokenLimitToggle = 40
 	const tokenList = await getTokenList(chainID)
 
+	console.log("Supported token addresses count:", tokenList.length)
 	const hecBridgeSplitterFactory = await ethers.getContractFactory("HecBridgeSplitter");
 	console.log("Deploying HecBridgeSplitter Contract...");
+
+	// const proxyAdmin = await hecBridgeSplitterFactory.attach("0x702408e2502792c8A13cc3096B4A14872eF52A98")
+
+	// for(let i = 0; i < Math.floor(tokenList.length / tokenLimitToggle) + 1; i ++){
+	// 	let tokens
+	// 	if(tokenLimitToggle * (i + 1) - 1 > tokenList.length) 
+	// 		tokens = tokenList.slice(i * tokenLimitToggle, tokenList.length)
+	// 	else tokens = tokenList.slice(i * tokenLimitToggle, (i + 1) * tokenLimitToggle )
+	// 	let toggleSuccess = false
+	// 	while (!toggleSuccess) {
+	// 		try{
+	// 			const tx1 = await proxyAdmin.connect(deployer).toggleMany(MANAGING.RESERVE_BRIDGE_ASSETS, tokens);
+	// 			await tx1.wait();
+	// 			console.log("Toggle tokens", i + 1, "out of ", Math.floor(tokenList.length / tokenLimitToggle) + 1)
+	// 			toggleSuccess = true
+	// 		}catch(error){
+	// 			console.error("Toggle transaction failed. Retrying...");
+	// 			console.log(error)
+	// 			console.log(tokens)
+	// 		}
+	// 		await waitSeconds(5);
+	// 	}
+	// 	await waitSeconds(5);
+	// }
+		
 
 	const hecBridgeSplitterContract = await hre.upgrades.deployProxy(
 		hecBridgeSplitterFactory,
@@ -54,14 +82,47 @@ async function main() {
 	await waitSeconds(3);
 	await hecBridgeSplitterContract.connect(deployer).toggleMany(MANAGING.RESERVE_BRIDGES, [lifiBridge, squidRouter]);
 
-	for(let i = 0; i < tokenList.length / tokenLimit + 1; i ++){
-		const tokens = tokenList.slice(i * tokenLimit, tokenLimit * (i + 1) - 1)
-		const tx = await hecBridgeSplitterContract.connect(deployer).queueMany(MANAGING.RESERVE_BRIDGE_ASSETS, tokens);
-		await tx.wait();
-		await waitSeconds(3);
-		await hecBridgeSplitterContract.connect(deployer).toggleMany(MANAGING.RESERVE_BRIDGE_ASSETS, tokens);
+	for(let i = 0; i < Math.floor(tokenList.length / tokenLimitQueue) + 1; i ++){
+		let tokens
+		if(tokenLimitQueue * (i + 1) - 1 > tokenList.length) 
+			tokens = tokenList.slice(i * tokenLimitQueue, tokenList.length)
+		else tokens = tokenList.slice(i * tokenLimitQueue, tokenLimitQueue * (i + 1))
+		
+		let queueSuccess = false;
+		while (!queueSuccess) {
+			try {
+				const tx = await hecBridgeSplitterContract.connect(deployer).queueMany(MANAGING.RESERVE_BRIDGE_ASSETS, tokens);
+				await tx.wait();
+				queueSuccess = true;
+				console.log("Queue tokens", i + 1, "out of ", Math.floor(tokenList.length / tokenLimitQueue) + 1, "from",  i * tokenLimitQueue, "to",  tokenLimitQueue * (i + 1))
+			} catch (error) {
+				console.error("Queue transaction failed. Retrying...");
+			}
+			await waitSeconds(5);
+		}
+		await waitSeconds(5);
 	}
 
+	for(let i = 0; i < Math.floor(tokenList.length / tokenLimitToggle) + 1; i ++){
+		let tokens
+		if(tokenLimitToggle * (i + 1) - 1 > tokenList.length) 
+			tokens = tokenList.slice(i * tokenLimitToggle, tokenList.length)
+		else tokens = tokenList.slice(i * tokenLimitToggle, tokenLimitToggle * (i + 1))
+		let toggleSuccess = false
+		while (!toggleSuccess) {
+			try{
+				const tx1 = await hecBridgeSplitterContract.connect(deployer).toggleMany(MANAGING.RESERVE_BRIDGE_ASSETS, tokens);
+				await tx1.wait();
+				console.log("Toggle tokens", i + 1, "out of ", Math.floor(tokenList.length / tokenLimitToggle) + 1, "from",  i * tokenLimitQueue, "to",  tokenLimitQueue * (i + 1))
+				toggleSuccess = true
+			}catch(error){
+				console.error("Toggle transaction failed. Retrying...");
+			}
+			await waitSeconds(5);
+		}
+		await waitSeconds(5);
+	}
+		
 }
 
 main().catch((error) => {
--- bridge/scripts/getTokenAddress.ts
+++ bridge/scripts/getTokenAddress.ts
@@ -1,3 +1,4 @@
+import { ethers } from "hardhat";
 
 const lifiApiUrl = 'https://li.quest/v1/tokens';
 const squidApiUrl = 'https://api.0xsquid.com/v1/tokens'
@@ -8,19 +9,32 @@ export const getTokenList =async (chainID: string) => {
     let lifiTokenLIst = await response.json()
 
     for(let i = 0; i < lifiTokenLIst.tokens[chainID].length; i ++){
-        temp.push(lifiTokenLIst.tokens[chainID][i].address)
+        if(ethers.utils.isAddress(lifiTokenLIst.tokens[chainID][i].address))
+            temp.push(lifiTokenLIst.tokens[chainID][i].address)
     }
 
     const response1 = await fetch(squidApiUrl)
     let squidTokenLIst = await response1.json()
 
     for(let i = 0 ; i < squidTokenLIst.tokens.length; i ++){
-        if(squidTokenLIst.tokens[i].chainId == chainID)  temp.push(squidTokenLIst.tokens[i].address)
+        if(squidTokenLIst.tokens[i].chainId == chainID) 
+            if(squidTokenLIst.tokens[i].address) temp.push(squidTokenLIst.tokens[i].address)
     }
+    
     const uniqueArray = temp.filter(function(elem, pos) {
         return temp.indexOf(elem) == pos;
     })
 
-    return uniqueArray
+    return removeZeroAddress(uniqueArray)
 }
 
+const removeZeroAddress = (uniqueArray: Array<string>) =>{
+    for(let i = 0; i < uniqueArray.length; i ++){
+        if(uniqueArray[i] == "0x0000000000000000000000000000000000000000" || uniqueArray[i] ==  "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE"){
+            if (i > -1) {
+                uniqueArray.splice(i, 1);
+            }
+        }
+    }
+    return uniqueArray
+}
