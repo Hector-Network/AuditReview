665250be99644f7b54e3df02511ddd6cd0ae4f7d
xiangdev1016 <xiangchu1016@gmail.com>
2023-07-17 20:06:15 +0200

    upd: add more console on testing

--- bridge/scripts/test/test.certik.ts
+++ bridge/scripts/test/test.certik.ts
@@ -3,18 +3,17 @@ require('dotenv').config();
 
 async function main() {
     const HecBridgeSplitterFatory = await ethers.getContractFactory("HecBridgeSplitterTest")
-    console.log("Deploying HecBridgeSplitter Implementation...");
-    const hecBridgeSplitterContract =  await HecBridgeSplitterFatory.deploy()
-    console.log("setting up")
+    const hecBridgeSplitterContract = await HecBridgeSplitterFatory.deploy()
     let tx1 = await hecBridgeSplitterContract.setUp()
     await tx1.wait()
-    console.log("testing ... ")
-    let tx2 = await hecBridgeSplitterContract.testBridge({value: "2000000000000000000"})
+    console.log("Did set up DAO wallet and dest count")
+    let tx2 = await hecBridgeSplitterContract.testBridge({ value: "2000000000000000000" })
     await tx2.wait()
-
+    console.log("Checked if call target address should receive 1 eth")
+    console.log("Testing succeeded")
 }
 
 main().catch((error) => {
-	console.error(error);
-	process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
